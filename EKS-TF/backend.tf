59a06338b346a15b056d39422afcc68b33ea55fb
santoshgadi <santoshpuli61@gmail.com>
2025-04-14 13:13:53 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "Mario-Game" # Replace with your actual S3 bucket name
+    bucket = "santoshgame" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
